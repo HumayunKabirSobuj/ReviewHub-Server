6ce9a7b0e43906cfafd675fb0260e45eff2fad89
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-04-30 18:37:06 +0600

    Review Add Logic added

--- src/app/modules/Review/review.route.ts
+++ src/app/modules/Review/review.route.ts
@@ -5,7 +5,11 @@ import RoleValidation from "../../middlewares/RoleValidation";
 import { reviewController } from "./review.controller";
 const router = express.Router();
 
-// router.get("/", CategoryController.getAllCategory);
+router.get(
+  "/",
+  RoleValidation(UserRole.ADMIN, UserRole.USER),
+  reviewController.getAllReview
+);
 // router.get("/:id", CategoryController.getSingleCategory);
 
 router.post(
